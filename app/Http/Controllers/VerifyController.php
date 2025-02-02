9f0d3517dc5d225ea343fdd81145ccfb2dea5cac
Sameed <sameedhassan22@gmail.com>
2025-02-02 13:45:21 +0500

    Remove AuthController and reset password methods from VerifyController

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -45,47 +45,6 @@ class VerifyController extends Controller
             ], 200)
             : view('auth.verified-email');
     }
-    
-    public function resetPassForm(Request $request, $token = null)
-    {
-        return view('auth.reset-password')->with([
-            'token' => $token,
-            'email' => $request->email,
-        ]);
-    }
-
-    public function NewPassword(Request $request)
-    {
-        $request->validate([
-            'token' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|min:8|confirmed'
-        ]);
-
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user) use ($request) {
-                $user->forceFill([
-                    'password' => Hash::make($request->password),
-                    'remember_token' => Str::random(60),
-                ])->save();
-
-                event(new PasswordReset($user));
-            }
-        );
-
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
-        return $status == Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
-            : back()->withInput($request->only('email'))
-            ->withErrors(['email' => __($status)]);
-    }
-
 
     public function viewEmail($id, $hash)
     {
